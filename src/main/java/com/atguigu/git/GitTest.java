eee0b2fa511356905ade43b4995588c5f7dd2608
ccb <99924595@qq.com>
2021-12-04 01:21:56 +0800

    master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,7 +10,7 @@ public class GitTest {
 
         System.out.println("hello git00");
         System.out.println("hello git00");
-        System.out.println("master test");
+        System.out.println("master test111");
 
     }
 }
