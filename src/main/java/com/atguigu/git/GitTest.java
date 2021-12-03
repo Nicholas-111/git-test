b1c0f8895d0e443de6c6b42e0302994cf2da9d6d
ccb <99924595@qq.com>
2021-12-04 01:13:21 +0800

    0000000

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,8 +8,8 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
 
-        System.out.println("hello git");
-        System.out.println("hello git");
+        System.out.println("hello git00");
+        System.out.println("hello git00");
 
     }
 }
